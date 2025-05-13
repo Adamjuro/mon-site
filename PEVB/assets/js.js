f3f7e64f4aa05223169bfd69ca6c67e1b558670c
Adamjuro <a.elmaftouhi2004@gmail.com>
2025-05-13 11:16:00 +0200

    Add files via upload

---
+++ PEVB/assets/js.js
@@ -0,0 +1,12 @@
+function imageCliquable() {
+  window.open("rizthaï.JPG")
+ }
+ 
+ function imageCliquable2() {
+  window.open("rizbasmati.JPG")
+ }
+ 
+  function imageCliquable3() {
+  window.open("rizeurope.JPG")
+ }
+ 
